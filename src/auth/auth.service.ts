74b2e918ab03d29e265f21d2e0e9c73610f5105f
Rizki Nuzul Muzaki <rizki.nm01@gmail.com>
2023-09-26 17:28:39 +0700

    Feat: auth service & unit test

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,63 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(registerDto: RegisterDto): Promise<{ token: string }> {
+    const { username, email, password } = registerDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await this.userModel.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      const token = this.jwtService.sign({ id: user._id });
+
+      return { token };
+    } catch (error) {
+      if (error?.code === 11000) {
+        throw new ConflictException('Duplicate Email Entered');
+      }
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<{ token: string }> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
+}
